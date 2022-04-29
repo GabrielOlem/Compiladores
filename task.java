e47f233bc78ede056c7aeea51c78606d1301e0cc
Gabriel Evangelista <45440229+GabrielOlem@users.noreply.github.com>
2022-04-29 00:24:03 -0300

    Task 2 Done

--- task.java
+++ task.java
@@ -1,52 +1,69 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.File;
 import java.util.*;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 public class task{
-    public static void main(String[] args) throws IOException{
-        Stack<String> pilha = new Stack<>();
-            // Creates a FileInputStream
-        FileInputStream file = new FileInputStream("/home/gme/Task1Comp/Calc1.stk");
+    public static void main(String[] args) throws Exception{
+        List<Token> pilha = new ArrayList<Token>();
+        File file = new File("/home/gme/Task1Comp/Calc1.stk");
 
-        // Creates a BufferedInputStream
-        BufferedInputStream input = new BufferedInputStream(file);
-
-        BufferedReader r = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
-        String i = r.readLine();
-        while (i != null) {
+        Scanner r = new Scanner(file);
+        while (r.hasNextLine()) {
+            String i = r.nextLine();
             if (i.matches("\\/|\\+|\\-|\\*")){
-                Double val1 = Double.parseDouble(pilha.pop());
-                Double val2 = Double.parseDouble(pilha.pop());
-
-                System.out.println("Operação a ser realizado " + val2 + i + val1);
                 if (i.equals("+")){
-                    pilha.push(Double.toString(val2 + val1));
+                    Token res = new Token(TokenType.PLUS, i);
+                    pilha.add(res);
                 }
                 else if (i.equals("-")){
-                    pilha.push(Double.toString(val2 - val1));
+                    Token res = new Token(TokenType.MINUS, i);
+                    pilha.add(res);
                 }
                 else if (i.equals("*")){
-                    pilha.push(Double.toString(val2 * val1));
+                    Token res = new Token(TokenType.STAR, i);
+                    pilha.add(res);
                 }
                 else if (i.equals("/")){
-                    if (val1 != 0){
-                        pilha.push(Double.toString(val2 / val1));
+                    Token res = new Token(TokenType.SLASH, i);
+                    pilha.add(res);
+                }
+            }
+            else if(i.matches("\\d+")){
+                Token num = new Token(TokenType.NUM, i);
+                System.out.println(num);
+                pilha.add(num);
+            }
+            else{
+                throw new Exception("Error: Unexpected character:" + i);
+            }
+        }
+        while(pilha.size() != 1){
+            Token top = pilha.remove(0);
+            if(top.type == TokenType.NUM){
+                Token top2 = pilha.remove(0);
+                Token op = pilha.remove(0);
+                Double n1 = Double.parseDouble(top2.lexeme);
+                Double n2 = Double.parseDouble(top.lexeme);
+                System.out.println("Operação a ser realizada " + top.lexeme + op.lexeme + top2.lexeme);
+                if (op.lexeme.equals("+")){
+                    pilha.add(0, new Token(TokenType.NUM, Double.toString(n2 + n1)));
+                }
+                else if (op.lexeme.equals("-")){
+                    pilha.add(0, new Token(TokenType.NUM, Double.toString(n2 - n1)));
+                }
+                else if (op.lexeme.equals("*")){
+                    pilha.add(0, new Token(TokenType.NUM, Double.toString(n2 * n1)));
+                }
+                else if (op.lexeme.equals("/")){
+                    if (n1 != 0){
+                        pilha.add(0, new Token(TokenType.NUM, Double.toString(n2 / n1)));
                     }
                     else{
                         throw new ArithmeticException("Divisão por 0 não possível");
                     }
                 }
             }
-            else{
-                pilha.push(i);
-            }
-            i = r.readLine();
         }
         r.close();
-        System.out.println("Valor calculado final: " + pilha.pop());
+        System.out.println("Valor calculado final: " + pilha.remove(0).lexeme);
     }
 }
